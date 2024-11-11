476f2a018f4d95de2f9f27312907c0bca2d6d46d
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2024-11-12 01:58:33 +0530

    many: add translations support

---
+++ Messages.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2024 Soumyadeep Ghosh <soumyadghosh@ubuntu.com>
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+$XGETTEXT $(find . -name "*.cpp" -o -name "*.qml") -o $podir/kcm_snap.pot
--- src/snapbackend.cpp
+++ src/snapbackend.cpp
@@ -4,9 +4,9 @@
  */
 
 #include "snapbackend.h"
+#include <KLocalizedString>
 #include <QDBusInterface>
 #include <QtQml>
-#include <qstringliteral.h>
 
 SnapBackend::SnapBackend()
 {
@@ -67,6 +67,16 @@ SnapBackend::SnapBackend()
     // Print results
 }
 
+/**
+ * Returns a list of KCMSnap objects filtered by the given filter string.
+ *
+ * If the filter string is empty, all snaps are returned. Otherwise, only
+ * snaps whose names contain the filter string, case-insensitively, are
+ * included in the returned list.
+ *
+ * @param filter The filter string to match snap names against.
+ * @return A list of filtered KCMSnap objects.
+ */
 const QList<KCMSnap *> SnapBackend::snaps(const QString &filter) const
 {
     QList<KCMSnap *> filteredSnaps;
@@ -89,6 +99,16 @@ const QList<KCMSnap *> SnapBackend::snaps(const QString &filter) const
 //     return m_slotSnaps;
 // }
 
+/**
+ * Connect a plug from plug_snap with name plug_name to a slot from
+ * slot_snap with name slot_name. If the connection is successful,
+ * returns an empty string. Otherwise returns an error message.
+ *
+ * @param plug_snap
+ * @param plug_name
+ * @param slot_snap
+ * @param slot_name
+ */
 QString SnapBackend::connectPlug(const QString &plug_snap, const QString &plug_name, const QString &slot_snap, const QString &slot_name) const
 {
     QSnapdClient client;
@@ -100,6 +120,16 @@ QString SnapBackend::connectPlug(const QString &plug_snap, const QString &plug_n
     return QString();
 }
 
+/**
+ * Disconnect a plug from plug_snap with name plug_name from a slot from
+ * slot_snap with name slot_name. If the disconnection is successful,
+ * returns an empty string. Otherwise returns an error message.
+ *
+ * @param plug_snap
+ * @param plug_name
+ * @param slot_snap
+ * @param slot_name
+ */
 QString SnapBackend::disconnectPlug(const QString &plug_snap, const QString &plug_name, const QString &slot_snap, const QString &slot_name) const
 {
     QSnapdClient client;
@@ -111,11 +141,24 @@ QString SnapBackend::disconnectPlug(const QString &plug_snap, const QString &plu
     return QString();
 }
 
+/**
+ * Compares two snaps by name. Returns true if a's name is less than b's name, false otherwise.
+ *
+ * @param a the first snap to compare
+ * @param b the second snap to compare
+ */
 bool SnapBackend::comparebyName(QSnapdSnap *a, QSnapdSnap *b)
 {
     return a->name() < b->name();
 }
 
+/**
+ * Returns true if the given snap has any app with a desktop file, false otherwise.
+ *
+ * This is used to determine if a snap can be launched from the kcm.
+ *
+ * @param snap the snap to check
+ */
 bool SnapBackend::invokAble(QSnapdSnap *snap)
 {
     bool invokable = false;
@@ -128,6 +171,13 @@ bool SnapBackend::invokAble(QSnapdSnap *snap)
     return invokable;
 }
 
+/**
+ * Launches the given snap if it has a desktop file.
+ *
+ * This is used when the user wants to launch a snap from the kcm.
+ *
+ * @param snap the snap to launch
+ */
 void SnapBackend::invokeDesktopApp(QSnapdSnap *snap) const
 {
     QString desktop;
@@ -144,95 +194,108 @@ void SnapBackend::invokeDesktopApp(QSnapdSnap *snap) const
     interface.call(QStringLiteral("OpenDesktopEntry"), desktop);
 }
 
+/**
+ * Returns a human-readable label for the given plug interface.
+ *
+ * This function maps a plug's interface name to a corresponding
+ * description that explains what access or functionality the plug
+ * provides. If the interface name is not recognized, the function
+ * returns the plug's name in lowercase as a fallback.
+ *
+ * @param plug A pointer to the QSnapdPlug object representing the plug
+ *             for which the label is requested.
+ * @return A QString containing the user-friendly label or the plug
+ *         name in lowercase if the interface is not found.
+ */
 const QString SnapBackend::plugLabel(const QSnapdPlug *plug)
 {
     static const QMap<QString, QString> interfaceLabels = {
-        {QStringLiteral("account-control"), QStringLiteral("Add user accounts and change passwords")},
-        {QStringLiteral("accounts-service"), QStringLiteral("Allows communication with the accounts service, such as GNOME Online Accounts")},
-        {QStringLiteral("alsa"), QStringLiteral("Play and record sound")},
-        {QStringLiteral("appstream-metadata"), QStringLiteral("Allows access to AppStream metadata")},
-        {QStringLiteral("audio-playback"), QStringLiteral("Play audio")},
-        {QStringLiteral("audio-record"), QStringLiteral("Record audio")},
-        {QStringLiteral("avahi-control"), QStringLiteral("Advertise services over the local network")},
-        {QStringLiteral("avahi-observe"), QStringLiteral("Detect network devices using mDNS/DNS-SD (Bonjour/zeroconf)")},
-        {QStringLiteral("bluetooth-control"), QStringLiteral("Access bluetooth hardware directly")},
-        {QStringLiteral("bluez"), QStringLiteral("Use bluetooth devices")},
-        {QStringLiteral("browser-support"), QStringLiteral("Use functions essential for Web browsers")},
-        {QStringLiteral("camera"), QStringLiteral("Use your camera")},
-        {QStringLiteral("cups"), QStringLiteral("Access to the CUPS socket for printing")},
-        {QStringLiteral("cups-control"), QStringLiteral("Print documents")},
-        {QStringLiteral("joystick"), QStringLiteral("Use any connected joystick")},
-        {QStringLiteral("desktop-launch"), QStringLiteral("Identify and launch desktop apps from other snaps")},
-        {QStringLiteral("docker"), QStringLiteral("Allow connecting to the Docker service")},
-        {QStringLiteral("firewall-control"), QStringLiteral("Configure network firewall")},
-        {QStringLiteral("fuse-support"), QStringLiteral("Setup and use privileged FUSE filesystems")},
-        {QStringLiteral("fwupd"), QStringLiteral("Update firmware on this device")},
-        {QStringLiteral("gsettings"), QStringLiteral("Provides access to any GSettings item for current user")},
-        {QStringLiteral("hardware-observe"), QStringLiteral("Access hardware information")},
-        {QStringLiteral("hardware-random-control"), QStringLiteral("Provide entropy to hardware random number generator")},
-        {QStringLiteral("hardware-random-observe"), QStringLiteral("Use hardware-generated random numbers")},
-        {QStringLiteral("home"), QStringLiteral("Access files in your home folder")},
-        {QStringLiteral("libvirt"), QStringLiteral("Access libvirt service")},
-        {QStringLiteral("locale-control"), QStringLiteral("Change system language and region settings")},
-        {QStringLiteral("location-control"), QStringLiteral("Change location settings and providers")},
-        {QStringLiteral("location-observe"), QStringLiteral("Access your location")},
-        {QStringLiteral("log-observe"), QStringLiteral("Read system and application logs")},
-        {QStringLiteral("lxd"), QStringLiteral("Access LXD service")},
-        {QStringLiteral("lxd-support"), QStringLiteral("Allows operating as the LXD service")},
-        {QStringLiteral("modem-manager"), QStringLiteral("Use and configure modems")},
-        {QStringLiteral("mount-observe"), QStringLiteral("Read system mount information and disk quotas")},
-        {QStringLiteral("mpris"), QStringLiteral("Control music and video players")},
-        {QStringLiteral("network"), QStringLiteral("Access the internet")},
-        {QStringLiteral("network-bind"),
-         QStringLiteral("Allows operating as a network service, enabling snap to "
-                        "run a server")},
-        {QStringLiteral("network-control"), QStringLiteral("Change low-level network settings")},
-        {QStringLiteral("network-manager"),
-         QStringLiteral("Access the NetworkManager service to read and change "
-                        "network settings")},
-        {QStringLiteral("network-manager-observe"), QStringLiteral("Allows observing NetworkManager settings")},
-        {QStringLiteral("network-observe"), QStringLiteral("Read access to network settings")},
-        {QStringLiteral("network-setup-control"), QStringLiteral("Change network settings")},
-        {QStringLiteral("network-setup-observe"), QStringLiteral("Read network settings")},
-        {QStringLiteral("network-status"), QStringLiteral("Access the NetworkStatus service")},
-        {QStringLiteral("ofono"),
-         QStringLiteral("Access the ofono service to read and change network "
-                        "settings for mobile telephony")},
-        {QStringLiteral("openvswitch"), QStringLiteral("Control Open vSwitch hardware")},
-        {QStringLiteral("optical-drive"), QStringLiteral("Read from CD/DVD")},
-        {QStringLiteral("password-manager-service"), QStringLiteral("Read, add, change, or remove saved passwords")},
-        {QStringLiteral("packagekit-control"), QStringLiteral("Control the PackageKit service")},
-        {QStringLiteral("pcscd"), QStringLiteral("Permits communication with PCSD smart card daemon")},
-        {QStringLiteral("ppp"),
-         QStringLiteral("Access pppd and ppp devices for configuring "
-                        "Point-to-Point Protocol connections")},
-        {QStringLiteral("process-control"), QStringLiteral("Pause or end any process on the system")},
-        {QStringLiteral("pulseaudio"), QStringLiteral("Play and record sound")},
-        {QStringLiteral("raw-usb"), QStringLiteral("Access USB hardware directly")},
-        {QStringLiteral("removable-media"), QStringLiteral("Read/write files on removable storage devices")},
-        {QStringLiteral("screen-inhibit-control"), QStringLiteral("Prevent screen sleep/lock")},
-        {QStringLiteral("serial-port"), QStringLiteral("Access serial port hardware")},
-        {QStringLiteral("shared-memory"), QStringLiteral("Enables two snaps to access the same shared memory")},
-        {QStringLiteral("shutdown"), QStringLiteral("Restart or power off the device")},
-        {QStringLiteral("snapd-control"), QStringLiteral("Install, remove and configure software (snaps)")},
-        {QStringLiteral("ssh-keys"), QStringLiteral("Access SSH private and public keys")},
-        {QStringLiteral("ssh-public-keys"), QStringLiteral("Access SSH public keys")},
-        {QStringLiteral("storage-framework-service"), QStringLiteral("Access Storage Framework service")},
-        {QStringLiteral("system-observe"), QStringLiteral("Read process and system information")},
-        {QStringLiteral("system-packages-doc"), QStringLiteral("Access system documentation in /usr/share/doc")},
-        {QStringLiteral("system-trace"), QStringLiteral("Monitor and control any running program")},
-        {QStringLiteral("time-control"), QStringLiteral("Change the date and time")},
-        {QStringLiteral("timeserver-control"), QStringLiteral("Change time server settings")},
-        {QStringLiteral("timezone-control"), QStringLiteral("Change the time zone")},
-        {QStringLiteral("udisks2"),
-         QStringLiteral("Access the UDisks2 service for configuring disks and "
-                        "removable media")},
-        {QStringLiteral("upower-observe"), QStringLiteral("Access energy usage data")},
-        {QStringLiteral("u2f-devices"), QStringLiteral("Read/write access to U2F devices exposed")}};
+        {QStringLiteral("account-control"), i18n("Add user accounts and change passwords")},
+        {QStringLiteral("accounts-service"), i18n("Allows communication with the accounts service, such as GNOME Online Accounts")},
+        {QStringLiteral("alsa"), i18n("Play and record sound")},
+        {QStringLiteral("appstream-metadata"), i18n("Allows access to AppStream metadata")},
+        {QStringLiteral("audio-playback"), i18n("Play audio")},
+        {QStringLiteral("audio-record"), i18n("Record audio")},
+        {QStringLiteral("avahi-control"), i18n("Advertise services over the local network")},
+        {QStringLiteral("avahi-observe"), i18n("Detect network devices using mDNS/DNS-SD (Bonjour/zeroconf)")},
+        {QStringLiteral("bluetooth-control"), i18n("Access bluetooth hardware directly")},
+        {QStringLiteral("bluez"), i18n("Use bluetooth devices")},
+        {QStringLiteral("browser-support"), i18n("Use functions essential for Web browsers")},
+        {QStringLiteral("camera"), i18n("Use your camera")},
+        {QStringLiteral("cups"), i18n("Access to the CUPS socket for printing")},
+        {QStringLiteral("cups-control"), i18n("Print documents")},
+        {QStringLiteral("joystick"), i18n("Use any connected joystick")},
+        {QStringLiteral("desktop-launch"), i18n("Identify and launch desktop apps from other snaps")},
+        {QStringLiteral("docker"), i18n("Allow connecting to the Docker service")},
+        {QStringLiteral("firewall-control"), i18n("Configure network firewall")},
+        {QStringLiteral("fuse-support"), i18n("Setup and use privileged FUSE filesystems")},
+        {QStringLiteral("fwupd"), i18n("Update firmware on this device")},
+        {QStringLiteral("gsettings"), i18n("Provides access to any GSettings item for current user")},
+        {QStringLiteral("hardware-observe"), i18n("Access hardware information")},
+        {QStringLiteral("hardware-random-control"), i18n("Provide entropy to hardware random number generator")},
+        {QStringLiteral("hardware-random-observe"), i18n("Use hardware-generated random numbers")},
+        {QStringLiteral("home"), i18n("Access files in your home folder")},
+        {QStringLiteral("libvirt"), i18n("Access libvirt service")},
+        {QStringLiteral("locale-control"), i18n("Change system language and region settings")},
+        {QStringLiteral("location-control"), i18n("Change location settings and providers")},
+        {QStringLiteral("location-observe"), i18n("Access your location")},
+        {QStringLiteral("log-observe"), i18n("Read system and application logs")},
+        {QStringLiteral("lxd"), i18n("Access LXD service")},
+        {QStringLiteral("lxd-support"), i18n("Allows operating as the LXD service")},
+        {QStringLiteral("modem-manager"), i18n("Use and configure modems")},
+        {QStringLiteral("mount-observe"), i18n("Read system mount information and disk quotas")},
+        {QStringLiteral("mpris"), i18n("Control music and video players")},
+        {QStringLiteral("network"), i18n("Access the internet")},
+        {QStringLiteral("network-bind"), i18n("Allows operating as a network service, enabling snap to run a server")},
+        {QStringLiteral("network-control"), i18n("Change low-level network settings")},
+        {QStringLiteral("network-manager"), i18n("Access the NetworkManager service to read and change network settings")},
+        {QStringLiteral("network-manager-observe"), i18n("Allows observing NetworkManager settings")},
+        {QStringLiteral("network-observe"), i18n("Read access to network settings")},
+        {QStringLiteral("network-setup-control"), i18n("Change network settings")},
+        {QStringLiteral("network-setup-observe"), i18n("Read network settings")},
+        {QStringLiteral("network-status"), i18n("Access the NetworkStatus service")},
+        {QStringLiteral("ofono"), i18n("Access the ofono service to read and change network settings for mobile telephony")},
+        {QStringLiteral("openvswitch"), i18n("Control Open vSwitch hardware")},
+        {QStringLiteral("optical-drive"), i18n("Read from CD/DVD")},
+        {QStringLiteral("password-manager-service"), i18n("Read, add, change, or remove saved passwords")},
+        {QStringLiteral("packagekit-control"), i18n("Control the PackageKit service")},
+        {QStringLiteral("pcscd"), i18n("Permits communication with PCSD smart card daemon")},
+        {QStringLiteral("ppp"), i18n("Access pppd and ppp devices for configuring Point-to-Point Protocol connections")},
+        {QStringLiteral("process-control"), i18n("Pause or end any process on the system")},
+        {QStringLiteral("pulseaudio"), i18n("Play and record sound")},
+        {QStringLiteral("raw-usb"), i18n("Access USB hardware directly")},
+        {QStringLiteral("removable-media"), i18n("Read/write files on removable storage devices")},
+        {QStringLiteral("screen-inhibit-control"), i18n("Prevent screen sleep/lock")},
+        {QStringLiteral("serial-port"), i18n("Access serial port hardware")},
+        {QStringLiteral("shared-memory"), i18n("Enables two snaps to access the same shared memory")},
+        {QStringLiteral("shutdown"), i18n("Restart or power off the device")},
+        {QStringLiteral("snapd-control"), i18n("Install, remove and configure software (snaps)")},
+        {QStringLiteral("ssh-keys"), i18n("Access SSH private and public keys")},
+        {QStringLiteral("ssh-public-keys"), i18n("Access SSH public keys")},
+        {QStringLiteral("storage-framework-service"), i18n("Access Storage Framework service")},
+        {QStringLiteral("system-observe"), i18n("Read process and system information")},
+        {QStringLiteral("system-packages-doc"), i18n("Access system documentation in /usr/share/doc")},
+        {QStringLiteral("system-trace"), i18n("Monitor and control any running program")},
+        {QStringLiteral("time-control"), i18n("Change the date and time")},
+        {QStringLiteral("timeserver-control"), i18n("Change time server settings")},
+        {QStringLiteral("timezone-control"), i18n("Change the time zone")},
+        {QStringLiteral("udisks2"), i18n("Access the UDisks2 service for configuring disks and removable media")},
+        {QStringLiteral("upower-observe"), i18n("Access energy usage data")},
+        {QStringLiteral("u2f-devices"), i18n("Read/write access to U2F devices exposed")}};
 
     return interfaceLabels.value(plug->interface(), plug->name().toCaseFolded());
 }
 
+/**
+ * Returns an icon name for a given plug name.
+ *
+ * This function returns an icon name from the Breeze icon theme that is
+ * associated with the given plug name. If the plug name is not found in the
+ * map, it returns a default icon name.
+ *
+ * @param plugName the name of the plug
+ * @return the icon name associated with the plug name
+ */
 const QString SnapBackend::plugIcon(const QString &plugName)
 {
     static const QMap<QString, QString> plugIcons = {{QStringLiteral("account-control"), QStringLiteral("user-identity")},
@@ -313,6 +376,13 @@ const QString SnapBackend::plugIcon(const QString &plugName)
     return plugIcons.value(plugName, QStringLiteral("dialog-question"));
 }
 
+/**
+ * Capitalize the given string, by uppercasing the first letter,
+ * and any letter that follows a hyphen.
+ *
+ * @param text the string to capitalize
+ * @return the capitalized string
+ */
 const QString SnapBackend::capitalize(const QString &text)
 {
     QString title = text;
